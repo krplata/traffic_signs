04cfb1fddfa57e2fef585734d8efe62428d5887d
krp97 <k.plata576@gmail.com>
2019-10-09 15:53:26 +0200

    Added histogram equalization before returning final input

--- src/pre_run.py
+++ src/pre_run.py
@@ -1,19 +1,22 @@
 import cv2
 
+# Functions used for preparing images to be used as an input to CNN.
+# Image of an arbitrary size -> downsampling -> histogram equalization -> CNN
 
-def resize_smaller_side(image_data, dest_size):
+
+def resize_smaller_side(image_data, dest_size, interpol=cv2.INTER_NEAREST):
     im_y, im_x = image_data.shape[:2]
     smaller_side = (im_y, dest_size) if im_x < im_y else (
         dest_size, im_x)
     return cv2.resize(
-        image_data, smaller_side, interpolation=cv2.INTER_NEAREST)
+        image_data, smaller_side, interpolation=interpol)
 
 
-def im_downsample(image_data, dest_size):
+def im_downsample(image_data, dest_size, interpol=cv2.INTER_NEAREST):
     im_x, im_y = image_data.shape[:2]
     if im_x == im_y:
         return cv2.resize(
-            image_data, (dest_size, dest_size), interpolation=cv2.INTER_NEAREST)
+            image_data, (dest_size, dest_size), interpolation=interpol)
     else:
         shrinked = resize_smaller_side(image_data, dest_size)
         shrinked_y, shrinked_x = shrinked.shape[:2]
@@ -22,6 +25,10 @@ def im_downsample(image_data, dest_size):
         return shrinked[bottom: bottom + dest_size, left: left + dest_size]
 
 
-# image = cv2.imread('./images/00000/00000_00013.ppm', cv2.IMREAD_COLOR)
-# print(image.shape)
-# cv2.imwrite('./output.png', im_downsample(image))
+def im_prepare(image_data):
+    downsampled = im_downsample(image_data, 31)
+    return cv2.equalizeHist(downsampled)
+
+
+# image = cv2.imread('../images/00000/00000_00013.ppm', cv2.IMREAD_COLOR)
+# cv2.imwrite('./output.png', im_downsample(image, 31))
