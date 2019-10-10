095a8c29648011241125b7162881db934e622888
krp97 <k.plata576@gmail.com>
2019-10-10 21:59:56 +0200

    Added splitting into train, test and validate

--- init_env.sh
+++ init_env.sh
@@ -3,7 +3,31 @@
 test_url=''
 training_url='https://sid.erda.dk/public/archives/daaeac0d7ce1152aea9b61d9f1e19370/GTSRB_Final_Training_Images.zip'
 fetch_url="$training_url"
-project_dir=$(realpath .)
+project_dir="$(realpath $(dirname $0))"
+
+if [[ -t 1 ]]; then
+    ncolors=$(tput colors)
+    if test -n "$ncolors" && test $ncolors -ge 8; then
+        bold="$(tput bold)"
+        normal="$(tput sgr0)"
+        black="$(tput setaf 0)"
+        red="$(tput setaf 1)"
+        green="$(tput setaf 2)"
+        yellow="$(tput setaf 3)"
+        blue="$(tput setaf 4)"
+        magenta="$(tput setaf 5)"
+        cyan="$(tput setaf 6)"
+        white="$(tput setaf 7)"
+    fi
+fi
+
+p_error() {
+    echo -e "${red}---> [Error]${white} $1"
+}
+
+p_info_msg() {
+    echo -e "${green}--->${white} $1"
+}
 
 while getopts ":tc:d:" opt; do
     case ${opt} in
@@ -19,9 +43,18 @@ while getopts ":tc:d:" opt; do
 done
 
 filename="$(echo $fetch_url | grep -Eo '[a-zA-Z0-9._]+$')"
-#wget $fetch_url
-#unzip -q "$project_dir/$filename"
+if [[ -f "$project_dir/$filename" ]]; then
+    p_info_msg "File already exists, skipping download"
+else
+    p_info_msg "Downloading $filename"
+    wget $fetch_url
+fi
+
+unzip -qu "$project_dir/$filename"
 python -m venv traffic_signs
 source traffic_signs/bin/activate
 pip install -r requirements.txt
-python $project_dir/src/prep_for_training.py
+
+p_info_msg "Setting up the dataset at: $project_dir/data"
+python $project_dir/src/setup.py --src "$project_dir/GTSRB/Final_Training/Images" \
+    --dest "$project_dir/data"
---
+++ src/setup.py
@@ -0,0 +1,53 @@
+import argparse
+import os
+import prep_for_training as prep
+import sys
+
+CLASS_SIZE = 3000
+
+
+def move_files(source_dir, dest_dir):
+    index = 0
+    for fname in os.listdir(source_dir):
+        if fname.endswith('.jpg'):
+            index += 1
+            src_image = os.path.join(source_dir, fname)
+            dst_image = os.path.join(dest_dir, fname)
+            os.rename(src_image, dst_image)
+        if index >= int(0.2*CLASS_SIZE):
+            break
+
+
+def recreate_dir_tree(source_dir, dest_dir):
+    for root, dirnames, filenames in os.walk(source_dir):
+        structure = os.path.join(dest_dir, root[len(source_dir):])
+        if not os.path.isdir(structure):
+            os.mkdir(structure)
+            move_files(root, structure)
+
+
+script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+parser = argparse.ArgumentParser()
+parser.add_argument('--src', dest='src_path',
+                    help='Sets the source path for recursive image augmentation.')
+parser.add_argument('--dest', dest='dest_path',
+                    help='Sets the output path for recursive image augmentation.')
+
+args = parser.parse_args()
+
+train_dir = os.path.join(args.dest_path, "train/")
+if not os.path.exists(train_dir):
+    os.makedirs(train_dir, exist_ok=True)
+
+if not os.path.exists(args.src_path):
+    print("Error: Invalid source path (prep_for_training.py)")
+    exit
+
+prep.ppm_dir_to_jpg(args.src_path, train_dir)
+prep.generate_augmented(train_dir, CLASS_SIZE)
+prep.cleanup_names(train_dir)
+
+validation_dir = os.path.join(args.dest_path, "validate/")
+test_dir = os.path.join(args.dest_path, "test/")
+recreate_dir_tree(train_dir, test_dir)
+recreate_dir_tree(train_dir, validation_dir)
