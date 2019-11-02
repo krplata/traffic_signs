417cc92e577087aaae4df748e210e0140c88edba
krp97 <k.plata576@gmail.com>
2019-11-02 11:25:00 +0100

    Initialized lfs for storing datasets

--- src/detection.py
+++ src/detection.py
@@ -6,6 +6,8 @@ from sklearn.model_selection import train_test_split
 import os
 from fnmatch import fnmatch
 from joblib import dump, load
+import time
+from sklearn.model_selection import cross_val_score
 
 
 class Feature_Extractor:
@@ -14,7 +16,7 @@ class Feature_Extractor:
         self.__args__ = args
         self.__kwargs__ = kwargs
 
-    def __yield_from_dir__(self, directory, ext):
+    def __yield_from_dir__(self, directory, ext, grayscale=False, dsize=None):
         '''
         Returns a generator of images from source_dir resized to dsize.
 
@@ -26,16 +28,20 @@ class Feature_Extractor:
             if fnmatch(filename, ext):
                 image = cv2.imread(os.path.join(
                     directory, filename), cv2.IMREAD_COLOR)
-                image = cv2.resize(image, (32, 32))
+                if dsize:
+                    image = cv2.resize(image, dsize)
+                if grayscale:
+                    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                image = cv2.Canny(image, 50, 200)
                 yield image
 
-    def dir_extract(self, dirpath, ext='*.ppm'):
+    def dir_extract(self, dirpath, ext='*.ppm', grayscale=False, dsize=None):
         '''
         Extracts features from all images within a directory,\
             using the function specified in Feature_Extractor __init__.
-        Returns a list of results of a 
+        Returns a list of results of a
         '''
-        generator = self.__yield_from_dir__(dirpath, ext)
+        generator = self.__yield_from_dir__(dirpath, ext, grayscale, dsize)
         results = []
         for image in generator:
             results.append(self.extract(image))
@@ -50,47 +56,59 @@ class SVM:
         if model_path:
             self.__clf__ = load(model_path)
         else:
-            self.__clf__ = svm.SVC(kernel='sigmoid', verbose=True)
+            self.__clf__ = svm.SVC(kernel=kernel, verbose=True)
 
-    def fit_n_test(self, x, y, split=0.2, *args, **kwargs):
+    def fit(self, x, y, split=0.2, *args, **kwargs):
         train_features, test_features, train_labels, test_labels = train_test_split(
             x,
             y,
             test_size=split,
             random_state=41)
         self.__clf__.fit(train_features, train_labels, *args, **kwargs)
-        return self.__test__(test_features, test_labels)
 
-    def __test__(self, test_x, test_y):
-        counter = 0
-        for index, tf in enumerate(test_x):
-            if self.__clf__.predict([tf]) == test_y[index]:
-                counter += 1
-        return (counter / len(test_y)) * 100
+    def est_cross_val(self, x, y, k=5):
+        return cross_val_score(self.__clf__, x, y, cv=k, n_jobs=-1)
 
     def save(self, filepath):
         dump(self.__clf__, filepath)
 
-    def predict(image, feature_gen):
+    def predict(self, image, feature_gen):
+        image = cv2.Canny(image, 50, 200)
         features = feature_gen.extract(image)
-        return self.__clf__.predict([featuress])
+        return self.__clf__.predict([features])
 
 
 def main():
     feature_extr = Feature_Extractor(hog, orientations=9, pixels_per_cell=(
-        4, 4), cells_per_block=(2, 2), multichannel=True)
+        8, 8), cells_per_block=(2, 2))
 
     print("Extracting features from the trainig set.")
-    features = feature_extr.dir_extract('./data/detection/train/positive')
+    features = feature_extr.dir_extract(
+        './data/detection/train/positive', dsize=(32, 32))
     classes = [1] * len(features)
-    neg_features = feature_extr.dir_extract('./data/detection/train/negative')
+    neg_features = feature_extr.dir_extract(
+        './data/detection/train/negative', dsize=(32, 32))
     classes.extend([0] * len(neg_features))
     features.extend(neg_features)
 
     clf = SVM()
     print("Fitting the SVM classifier.")
-    accuracy = clf.fit_n_test(features, classes)
-    print(f"Train-test accuracy: {accuracy}%")
+    accuracy = clf.est_cross_val(features, classes)
+    print(accuracy)
+    # clf = SVM(model_path='models/detection.joblib')
+    # image = cv2.imread('data/detection/stock/00000.ppm', cv2.IMREAD_COLOR)
+    # x, y, z = image.shape
+    # pyramid = Image_Pyramid(image, 0.7, (x*0.6, y*0.6))
+    # generator = pyramid.sliding_window((32, 32), (10, 10))
+    # start = time.time()
+    # count = 0
+    # for image in generator:
+    #     if clf.predict(image, feature_extr) == 1:
+    #         count += 1
+    #         print("Found")
+    #         cv2.imwrite(f"predictions/{count}.jpg", image)
+    # print(f"Count: {count}")
+    # print(time.time() - start)
 
 
 if __name__ == "__main__":
